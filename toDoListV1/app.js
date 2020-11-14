203770cf6279c8273d50002ca3726096b9788983
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-11-14 19:22:42 +0200

    Finished

--- toDoListV1/app.js
+++ toDoListV1/app.js
@@ -1,34 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const date = require(__dirname+"/date.js");
 
 var items = ["Code"];
+let workItewms = [];
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res)
 {
-    var today = new Date();
-
-    var options =
-    {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("un-US", options);
-
-    res.render("list", { kindOfDay: day, newListItems: items});
+    let day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items});
 })
 
 app.post("/", function(req,res)
 {
     var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/")
+    if (req.body.list==="Work") 
+    {
+        workItewms.push(item);
+        res.redirect("/work")
+    }
+    else
+    {
+        items.push(item);
+        res.redirect("/")
+    }
 });
+
+app.get("/work", function(req,res)
+{
+    res.render("list", {listTitle: "Work List", newListItems: workItewms})
+})
+
+app.post("/work", function(req,res)
+{
+    let item = req.body.newItem;
+    workItewms.push(item);
+    res.redirect("/work")
+})
 
 app.listen(3000, function ()
 {
