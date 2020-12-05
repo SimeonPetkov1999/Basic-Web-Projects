5bbd720a481f6867e3786f45756bbf81e8d75c07
SimeonPetkov1999 <simeonpetkov1999@gmail.com>
2020-12-05 14:20:51 +0200

    md5

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
-//process.env.SECRET
 //process.env.SECRET
 
 app.use(express.static("public"));
@@ -19,10 +19,9 @@ const userSchema = new mongoose.Schema(
     {
         email: String,
         password: String
-
     });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +44,7 @@ app.post("/register", function (req, res)
     const newUser = new User(
         {
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
     newUser.save(function (err)
     {
@@ -63,7 +62,7 @@ app.post("/register", function (req, res)
 app.post("/login", function (req, res)
 {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser)
     {
